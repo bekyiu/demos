5661136ca7cd19ccbdca5fc1034fffab7be603ed
bekyiu <651731457@qq.com>
2019-09-25 19:08:54 +0800

    zuul的基本使用

---
+++ zuul-gateway/src/main/java/bekyiu/AppZuul.java
@@ -0,0 +1,15 @@
+package bekyiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy // 开启zuul网关
+public class AppZuul
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AppZuul.class, args);
+    }
+}
