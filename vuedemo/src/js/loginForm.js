68be758c62cb25463b3e40f5e09228be6adcab95
bekyiu <651731457@qq.com>
2019-09-30 21:07:16 +0800

    vue入门的一些例子

---
+++ vuedemo/src/js/loginForm.js
@@ -0,0 +1,9 @@
+const loginForm = {
+    template: `
+        <div>
+            <h2>登陆</h2>
+            用户名: <input type="text">
+            密码: <input type="password">
+        </div>
+    `
+}
---
+++ vuedemo/src/js/registerForm.js
@@ -0,0 +1,10 @@
+const registerForm = {
+    template: `
+        <div>
+            <h2>注册</h2>
+            用户名: <input type="text">
+            密码: <input type="password">
+            确认密码: <input type="password">
+        </div>
+    `
+}
